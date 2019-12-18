353ec92bd02b7d96fcb37981a29d17155cb2f246
virginia rochester <virginia.rochester@worldvision.org.uk>
2019-12-18 14:55:05 +0000

    Dev 18th December

---
+++ StatNav.WebApplication/Scripts/common.js
@@ -0,0 +1,18 @@
+﻿// ensure all ajax requests are set with a unique id to prevent browser caching
+$.ajaxSetup({ cache: false });
+
+pathArray = location.href.split('/');
+protocol = pathArray[0];
+host = pathArray[2];
+
+if (host.indexOf('localhost') >= 0) {
+    url = protocol + '//' + host + '/';
+} else {
+    url = protocol + '//' + host + '/PALHub/';
+}
+
+$(document).ajaxError(function (event, jqXHR, settings, exception) {
+    if (jqXHR.status == 401) {  //Unathorised
+        window.location.replace(url + "Account/Login?timeout");
+    }
+});
