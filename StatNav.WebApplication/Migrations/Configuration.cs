ea748e7fda8633f49d200bc346d7d6df61cdc515
virginia rochester <virginia.rochester@worldvision.org.uk>
2020-01-31 08:24:16 +0000

    AB#907, AB#1052 updated following static analysis

--- StatNav.WebApplication/Migrations/Configuration.cs
+++ StatNav.WebApplication/Migrations/Configuration.cs
@@ -1,10 +1,7 @@
 ﻿namespace StatNav.WebApplication.Migrations
 {
-    using System;
-    using System.Data.Entity;
     using System.Data.Entity.Migrations;
-    using System.Linq;
-
+ 
     internal sealed class Configuration : DbMigrationsConfiguration<StatNav.WebApplication.DAL.StatNavContext>
     {
         public Configuration()
