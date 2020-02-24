d2c06f5850d7329edae2ad70561794dd17075729
ram kumar <ramkumar.rajendran@worldvision.org.uk>
2020-02-24 15:23:23 +0530

    Add Unit test package

--- StatNav.IntegrationTests/StatNavIntegrationTest.cs
+++ StatNav.IntegrationTests/StatNavIntegrationTest.cs
@@ -1,48 +1,48 @@
-﻿using NUnit.Framework;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Firefox;
-using OpenQA.Selenium.IE;
-using System;
-using System.Configuration;
-using TestAutomationFramework;
+﻿//using NUnit.Framework;
+//using OpenQA.Selenium;
+//using OpenQA.Selenium.Chrome;
+//using OpenQA.Selenium.Firefox;
+//using OpenQA.Selenium.IE;
+//using System;
+//using System.Configuration;
+//using TestAutomationFramework;
 
-namespace StatNav.IntegrationTests
-{
-    [TestFixture(typeof(FirefoxDriver))]
-    [TestFixture(typeof(ChromeDriver))]
-    //[TestFixture(typeof(InternetExplorerDriver))]
-    public class StatNavIntegrationTest<TWebDriver> where TWebDriver : IWebDriver, new() 
-    {
+//namespace StatNav.IntegrationTests
+//{
+//    [TestFixture(typeof(FirefoxDriver))]
+//    [TestFixture(typeof(ChromeDriver))]
+//    //[TestFixture(typeof(InternetExplorerDriver))]
+//    public class StatNavIntegrationTest<TWebDriver> where TWebDriver : IWebDriver, new() 
+//    {
        
-        [OneTimeSetUp]
-        public void Initialization()
-        {
+//        [OneTimeSetUp]
+//        public void Initialization()
+//        {
 
-            Utils.DriverSetup();
-            AppDriver.driver = new TWebDriver();
-            string Browser = Convert.ToString(AppDriver.driver);
-            string value = AppClass.Browsername(Browser);
-            Utils.Extent_Test(ConfigurationManager.AppSettings["ReportsPath"] + value + " Test Report.html");
-        }
+//            Utils.DriverSetup();
+//            AppDriver.driver = new TWebDriver();
+//            string Browser = Convert.ToString(AppDriver.driver);
+//            string value = AppClass.Browsername(Browser);
+//            Utils.Extent_Test(ConfigurationManager.AppSettings["ReportsPath"] + value + " Test Report.html");
+//        }
 
-        [OneTimeTearDown]
-        public void Browser()
-        {
-           AppDriver.extent.Flush();
-        }
+//        [OneTimeTearDown]
+//        public void Browser()
+//        {
+//           AppDriver.extent.Flush();
+//        }
 
-        [Test]
-        public void Program()
-        {
-            string Browser = Convert.ToString(AppDriver.driver);
-            string value = AppClass.Browsername(Browser);
-            Create_Program.CreateProgram(value);
-            //Delete_Program.DeleteProgram(value);
-            AppDriver.driver.Close();
-        }
-    }
-}
+//        [Test]
+//        public void Program()
+//        {
+//            string Browser = Convert.ToString(AppDriver.driver);
+//            string value = AppClass.Browsername(Browser);
+//            Create_Program.CreateProgram(value);
+//            //Delete_Program.DeleteProgram(value);
+//            AppDriver.driver.Close();
+//        }
+//    }
+//}
 
 
 
