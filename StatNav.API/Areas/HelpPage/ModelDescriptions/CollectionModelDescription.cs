b2455c99fa94324b1a6acd61d8eb8f899e6a46a7
virginia rochester <virginia.rochester@worldvision.org.uk>
2020-06-22 09:43:40 +0100

    Trying to delete the api project - local problems causing some delete issues

--- StatNav.API/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace StatNav.API.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- StatNav.API/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace StatNav.API.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
